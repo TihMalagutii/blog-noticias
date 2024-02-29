ed417391f2df2449573b65b9e4036532b89eeb2d
TihMalagutii <euphoriabeats7146090532@gmail.com>
2024-02-29 17:05:16 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/css/app.css",
+    "./resources/js/app.js",
+    "./resources/views/welcome.blade.php"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
