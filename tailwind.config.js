c2c116e74393f2859ac02c4b3d1454ffc47d0308
TihMalagutii <euphoriabeats7146090532@gmail.com>
2024-03-01 13:55:52 -0300

    mod(code): Adjusts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./resources/css/app.css",
     "./resources/js/app.js",
+    "./resources/js/post.js",
     "./resources/views/welcome.blade.php",
     "./resources/views/post.blade.php"
   ],
